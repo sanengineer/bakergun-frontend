f014b36f90c8a0018020943c90bc8b34f958fdaf
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-10-25 17:57:30 +0700

    update code

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,28 +2,37 @@ import React, { Component } from "react";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `https://bakergun-backend.herokuapp.com/api/v1/gameboard`,
+  baseURL: `https://bakergun-backend.herokuapp.com/api/v1/images`,
 });
 
 class Hero extends Component {
-  state = {
-    images: [],
-  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+    }
+  }
+  
 
   componentDidMount() {
     api.get("/").then((res) => {
-      console.log(res.data);
-      this.setState({ images: res.data });
+      if (res.data.length>0) {
+        console.log(res.data);
+        // this.setState({ images: res.data.map((image) => image.imageUrl) });
+        this.setState({ images: res.data });
+      }
     });
   }
 
   render() {
     return (
       <header className="App-header">
-        {this.state.images.map((images) => (
-          <img key={images.id} src={images.imageUrl} alt={images.name} />
-          // <h1 key={gameboard.id}>{gameboard.name}</h1>
-        ))}
+        {this.state.images.map((images) => {
+          return (
+          <img key={images.id} src={images.imageUrl}/>
+        )
+         })}
       </header>
     );
   }
